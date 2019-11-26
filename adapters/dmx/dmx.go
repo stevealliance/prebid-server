cbccbf55a7cd03b3197f469b0bc742109ea38c07
steve-a-districtm <steve@districtm.net>
2019-11-25 23:29:52 -0500

    adding openrtbExt dmx, finish the reques portion

--- adapters/dmx/dmx.go
+++ adapters/dmx/dmx.go
@@ -1,8 +1,16 @@
 package dmx
 
 import (
+	"github.com/prebid/prebid-server/errortypes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/mxmCherry/openrtb"
 	"github.com/prebid/prebid-server/adapters"
+	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
 const Seat = "dmx"
@@ -12,9 +20,14 @@ const endpoint = "https://dmx.districtm.io/b/v2"
 type DmxAdapter struct {
 }
 type dmxSite struct {
-	Domain  string      `json:"domain"`
-	Page    string      `json:"page"`
-	Content interface{} `json:content`
+	Domain    string       `json:"domain"`
+	Page      string       `json:"page"`
+	Content   interface{}  `json:content`
+	Publisher dmxPublisher `json:"publisher"`
+}
+
+type dmxPublisher struct {
+	ID string `json:"id"`
 }
 
 type dmxGeo struct {
@@ -27,58 +40,256 @@ type dmxGeo struct {
 type dmxDevice struct {
 	Ua         string `json:"ua"`
 	Devicetype int8   `json:"devicetype"`
-	Ip         string `json:"ip"`
+	IP         string `json:"ip"`
 	Ipv6       string `json:"ipv6"`
 	Geo        dmxGeo `json:"geo"`
 	Language   string `json:"language"`
 }
 
 type dmxUser struct {
-	Id      string `json:"id"`
-	Buyerid string `json:"buyerid"`
+	ID      string `json:"id,omitempty"`
+	BuyerID string `json:"buyerid,omitempty"`
 }
 
 type dmxImp struct {
-	Id       string    `json:"id"`
+	ID       string    `json:"id"`
 	Banner   dmxBanner `json:"banner"`
 	Tagid    string    `json:"tagid"`
 	Bidfloor float32   `json:"bidfloor"`
-	secure   int       `json:"secure"`
+	Secure   int8      `json:"secure"`
 }
 
 type dmxBanner struct {
-	W     int8   `json:"w"`
-	H     int8   `json:"h"`
-	Battr []int8 `json:"battr"`
+	W      uint64           `json:"w"`
+	H      uint64           `json:"h"`
+	Battr  []uint64         `json:"battr,omitempty"`
+	Format []openrtb.Format `json:"format,omitempty"`
+}
+
+type dmxFormat struct {
+	W uint64 `json:"w"`
+	H uint64 `json:"h"`
 }
 
 /**
 openrtb.User should be use in that interface
 */
 type dmxBannerRequest struct {
-	Bcat   []string  `json:"bcat"`
-	Badv   []string  `json:"badv"`
+	Bcat   []string  `json:"bcat,omitempty"`
+	Badv   []string  `json:"badv,omitempty"`
 	Site   dmxSite   `json:"site"`
-	Id     string    `json:"id"`
+	ID     string    `json:"id"`
 	Device dmxDevice `json:"device"`
 	User   dmxUser   `json:"user"`
 	Imp    []dmxImp  `json:"imp"`
 }
 
+type dmxResponse struct {
+	ID string `json:"id"`
+	ImpID string `json:"impid"`
+	Price float64 `json:"price"`
+	NUrl string		`json:"nurl,omitempty"`
+	BUrl string 	`json:"burl,omitempty"`
+	Adm string `json:"adm"`
+	H uint64 `json:"h"`
+	W uint64 `json:"w"`
+	ADomain []string `json:"adomain"`
+	Bundle string `json:"bundle,omitempty"`
+	IUrl string `json:"iurl,omitempty"`
+	Cid string `json:"cid"`
+	Crid string `json:"crid"`
+	Cat []string `json:"cat,omitempty"` 
+}
+
 func (dmx *DmxAdapter) MakeRequests(request *openrtb.BidRequest, reqData *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
-	// dmxRequests, _ := buildRequests(request)
+	fmt.Println("hello from steve 1")
+	dmxRequests, _ := preprocess(request)
+	errs := make([]error, 0)
 
-	// fmt.Print(dmxRequests)
+	headers := http.Header{}
+	headers.Add("Content-Type", "application/json;charset=utf-8")
+	headers.Add("Accept", "application/json")
+	reqArr := make([]*adapters.RequestData, 0)
 
+	fmt.Println(dmxRequests)
+	top := len(dmxRequests.Imp) > 0
+
+	for top {
+		resJSON, _ := json.Marshal(dmxRequests)
+		reqArr = append(reqArr, &adapters.RequestData{
+			Method:  "POST",
+			Uri:     endpoint,
+			Body:    resJSON,
+			Headers: headers,
+		})
+	}
+	return reqArr, errs
 }
 
 func (dmx *DmxAdapter) MakeBids(internalRequest *openrtb.BidRequest, externalRequest *adapters.RequestData, response *adapters.ResponseData) (*adapters.BidderResponse, []error) {
+	if response.StatusCode == http.StatusNoContent {
+		return nil, nil
+	}
 
+	if response.StatusCode == http.StatusBadRequest {
+		return nil, []error{&errortypes.BadInput{
+			Message: fmt.Sprintf("Unexpected status code %d. Run with request.debug for more info", response.StatusCOde)
+		}}
+	}
+
+	if response.StatusCode != http.StatusOk {
+		return nil, []error{fmt.Errorf("Unexpected status code %d. Run with request.debug for more info", response.StatusCOde)}
+	}
+
+	var bidResp = openrtb.BidResponse
+	if err := json.Unmarshal(response.Body, &bidResp); err != nil {
+		return nil, []error{err}
+	}
+
+	errs := make([]error, 0)
+	resData := adapters.NewBidderResponseWithBidsCapacity(5)
+
+	for _, bid := range bidResp.SeatBid {
+		for i:= 0; i < len(bid.Bid); i++ {
+			b := bid.Bid[i]
+			var bidExt 
+		}
+	}
+
+
+	return resData, errs
 }
 
-func buildRequests(request *openrtb.BidRequest) (reqs dmxBannerRequest, errs []error) {
-	var imps = make([]dmxImp, 0)
-	reqs.Imp = imps
+func preprocess(request *openrtb.BidRequest) (reqs dmxBannerRequest, errs []error) {
+
+	var imps = make([]openrtb.Imp, 0)
+
+	for i := 0; i < len(request.Imp); i++ {
+		if request.Imp[i].Banner != nil && ((request.Imp[i].Banner.Format[0].H != 0 && request.Imp[i].Banner.Format[0].W != 0) ||
+			(request.Imp[i].Banner.H != nil && request.Imp[i].Banner.W != nil)) {
+			imps = append(imps, request.Imp[i])
+		}
+	}
+	if len(imps) == 0 {
+		errs = append(errs, errors.New("No valid impressions was found"))
+		return
+	}
+	if len(imps) > 0 {
+		request.Imp = imps
+		reqs, errs = getBannerRequest(request)
+	}
 
 	return
+}
+
+func getBannerRequest(request *openrtb.BidRequest) (dmxBannerRequest, []error) {
+	var dmx dmxBannerRequest
+	var errs = make([]error, 0, len(request.Imp))
+	var dmxExt openrtb_ext.ExtDmx
+	var bidderExt adapters.ExtImpBidder
+	if err := json.Unmarshal(bidderExt.Bidder, &dmxExt); err != nil {
+		errs = append(errs, err)
+		return dmx, errs
+	}
+	dmx.Site = dmxSite{
+		Domain: getDomain(request.Site.Page),
+		Page:   request.Site.Page,
+		Publisher: dmxPublisher{
+			ID: dmxExt.MemberID,
+		},
+	}
+
+	if request.User != nil && request.User.ID != "" {
+		if dmx.User.ID == "" {
+			dmx.User.ID = request.User.ID
+		}
+	}
+
+	if request.User != nil && request.User.BuyerUID != "" {
+		if dmx.User.BuyerID == "" {
+			dmx.User.BuyerID = request.User.BuyerUID
+		}
+	}
+
+	dmx.ID = request.ID
+	Imp := make([]dmxImp, 0)
+	for i := 0; i < len(request.Imp); i++ {
+
+		// beachfrontExt, err := getBeachfrontExtension(request.Imp[i])
+
+		// if err != nil {
+		// 	errs = append(errs, err)
+		// 	continue
+		// }
+
+		// appid, err := getAppId(beachfrontExt, openrtb_ext.BidTypeBanner)
+
+		// if err != nil {
+		// 	// Failed to get an appid, so this request is junk.
+		// 	errs = append(errs, err)
+		// 	continue
+		// }
+		var sizes dmxBanner
+
+		sizes = dmxBanner{
+			Format: request.Imp[i].Banner.Format,
+			H:      request.Imp[i].Banner.Format[0].H,
+			W:      request.Imp[i].Banner.Format[0].W,
+		}
+
+		Imp = append(Imp, dmxImp{
+			ID:     request.Imp[i].ID,
+			Secure: 1,
+			Tagid:  dmxExt.TagID,
+			Banner: sizes,
+		})
+
+		// if beachfrontExt.BidFloor <= minBidFloor {
+		// 	slot.Bidfloor = 0
+		// }
+
+		// dmx.Imp = append(dmx.Imp, Imp)
+	}
+	dmx.Imp = Imp
+
+	if len(dmx.Imp) == 0 {
+		return dmx, errs
+	}
+
+	return dmx, errs
+}
+
+func getDomain(page string) string {
+	protoURL := strings.Split(page, "//")
+	var domainPage string
+
+	if len(protoURL) > 1 {
+		domainPage = protoURL[1]
+	} else {
+		domainPage = protoURL[0]
+	}
+
+	return strings.Split(domainPage, "/")[0]
+
+}
+func isSecure() int8 {
+	return 1
+}
+
+func getIP(ip string) string {
+	// This will only effect testing. The backend will return "" for localhost IPs,
+	// and seems not to know what IPv6 is, so just setting it to one that is not likely to
+	// be used.
+	if ip == "" || ip == "::1" || ip == "127.0.0.1" {
+		return "192.168.255.255"
+	}
+	return ip
+}
+
+func getBidType(externalRequest *adapters.RequestData) openrtb_ext.BidType {
+	return openrtb_ext.BidTypeBanner
+}
+
+func NewDmxBidder() *DmxAdapter {
+	return &DmxAdapter{}
 }
--- adapters/dmx/usersync_test.go
+++ adapters/dmx/usersync_test.go
@@ -2,10 +2,18 @@ package dmx
 
 import (
 	"testing"
+	"text/template"
 
-	"github.com/prebid/prebid-server/adapters/adapterstest"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestJsonSamples(t *testing.T) {
-	adapterstest.RunJSONBidderTest(t, "dmxtest", new(DmxAdapter))
+func TestDmxSyncer(t *testing.T) {
+	temp := template.Must(template.New("sync-template").Parse("localhost"))
+	syncer := NewDmxSyncer(temp)
+	syncInfo, err := syncer.GetUsersyncInfo("", "")
+	assert.NoError(t, err)
+	assert.Equal(t, "localhost", syncInfo.URL)
+	assert.Equal(t, "iframe", syncInfo.Type)
+	assert.EqualValues(t, 0, syncer.GDPRVendorID())
+	assert.Equal(t, false, syncInfo.SupportCORS)
 }
